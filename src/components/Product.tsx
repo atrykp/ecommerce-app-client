a0f7a627d25235cce343a723a0513987be729745
Patryk Plaza <atrykp0@gmail.com>
2021-05-22 19:07:23 +0200

    add products list in HomeScreen

---
+++ src/components/Product.tsx
@@ -0,0 +1,43 @@
+import { Card } from "react-bootstrap";
+import products from "../products";
+
+interface IProduct {
+  product: {
+    _id: string;
+    name: string;
+    image: string;
+    description: string;
+    brand: string;
+    category: string;
+    price: number;
+    countInStock: number;
+    rating: number;
+    numReviews: number;
+  };
+}
+
+const Product = ({ product }: IProduct) => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </a>
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+        <Card.Text as="div">
+          {" "}
+          <div className="my-3">
+            {product.rating} from {product.numReviews} reviews
+          </div>
+        </Card.Text>
+        <Card.Text as="h3">${product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  );
+};
+
+export default Product;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,20 @@
+import { Col, Row } from "react-bootstrap";
+import Product from "../components/Product";
+import products from "../products";
+
+const HomeScreen = () => {
+  return (
+    <>
+      <h1>Latest Products</h1>
+      <Row>
+        {products.map((prod) => (
+          <Col key={prod.name} sm={12} md={6} lg={4} xl={3}>
+            <Product product={prod} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
+};
+
+export default HomeScreen;
